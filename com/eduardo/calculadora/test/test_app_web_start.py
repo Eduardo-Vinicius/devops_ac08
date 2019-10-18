8d5bcae5e7974d8f33b2d8094cdc1b03c78f35a7
Leonardo Buzzo <1900861@SALAS.AULAS>
2019-10-18 20:29:56 -0300

    vai senhor

--- com/eduardo/calculadora/test/test_app_web_start.py
+++ com/eduardo/calculadora/test/test_app_web_start.py
@@ -9,9 +9,9 @@ def test_root_status():
     assert response.status_code == 200, 'Deveria existir essa rota'
 
 def test_root_url():
-  '''
-  Função que testa a URL do root
-  '''
+    '''
+    Função que testa a URL do root
+    '''
     instancia_app = app_web_start.app.test_client()
     response = instancia_app.get('/')
     assert response.data.decode('utf-8') == 'Index Page!', 'Deveria ser Index Page'
